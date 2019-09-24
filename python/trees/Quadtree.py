d65ecdbf14724329aa13de7c4efac8155378d994
Sluchaynaya Kotya <sluchaynayakotya@gmail.com>
2019-09-24 16:10:55 +0300

    module was renamed

--- python/trees/Quadtree.py
+++ python/trees/Quadtree.py
@@ -3,24 +3,20 @@ from types import SimpleNamespace
 
 
 class Quadtree:
-    def __init__(self):
-        self.raw = None
-
-
     def __init__(self, points, rect, depth):
-        self.raw = self.generate(points, rect, depth)
+        self.raw = self.__generate(points, rect, depth)
 
 
-    def in_rect(self, point, rect):
+    def __in_rect(self, point, rect):
         """ Tests is point in rect or not. """
         return rect[0] <= point[0] and point[0] <= rect[0] + rect[2] and \
                rect[1] <= point[1] and point[1] <= rect[1] + rect[3]
 
 
-    # points -- list(list(x, y)[, ...])                    :: floats, in range [0,1)
-    # rect   -- list(left, top, width, height)             :: floats
-    # depth  -- nesting iterator (approximation accurancy) :: int
-    def generate(self, points, rect, depth):
+    # points -- list(list(x, y)[, ...]), floats in range [0,1)
+    # rect   -- list(left, top, width, height), floats
+    # depth  -- nesting iterator (approximation accurancy), int
+    def __generate(self, points, rect, depth):
         depth -= 1
         if len(points) == 0 or depth < 0: return None
 
@@ -44,16 +40,16 @@ class Quadtree:
         points_LB = []
         points_RB = []
         for point in points:
-            if   self.in_rect(point, rect_LT): points_LT.append(point)
-            elif self.in_rect(point, rect_RT): points_RT.append(point)
-            elif self.in_rect(point, rect_LB): points_LB.append(point)
-            elif self.in_rect(point, rect_RB): points_RB.append(point)
+            if   self.__in_rect(point, rect_LT): points_LT.append(point)
+            elif self.__in_rect(point, rect_RT): points_RT.append(point)
+            elif self.__in_rect(point, rect_LB): points_LB.append(point)
+            elif self.__in_rect(point, rect_RB): points_RB.append(point)
 
         return {
             'points': points,
             'rect': rect,
-            'LT': self.generate(points_LT, rect_LT, depth),
-            'RT': self.generate(points_RT, rect_RT, depth),
-            'LB': self.generate(points_LB, rect_LB, depth),
-            'RB': self.generate(points_RB, rect_RB, depth),
+            'LT': self.__generate(points_LT, rect_LT, depth),
+            'RT': self.__generate(points_RT, rect_RT, depth),
+            'LB': self.__generate(points_LB, rect_LB, depth),
+            'RB': self.__generate(points_RB, rect_RB, depth),
         }
