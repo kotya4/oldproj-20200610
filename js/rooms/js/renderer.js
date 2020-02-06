ab8d2939f961dfb16d904eb9660a112dac6614f7
Sluchaynaya Kotya <sluchaynayakotya@gmail.com>
2020-02-06 12:54:07 +0300

    roomrenderer

--- js/rooms/js/renderer.js
+++ js/rooms/js/renderer.js
@@ -1,10 +1,7 @@
 //
 function Renderer(g) {
 
-  function arrpart(a, start, count) {
-    return a.slice(start, start + count);
-  }
-
+  // Init.
 
   const { gl, ctx, webgl } = g;
 
@@ -19,92 +16,51 @@ function Renderer(g) {
     }
   };
 
+  // Room defined.
 
+  const rects = [
+    [ 0, 0,  3, 0],
+    [ 3, 0,  3, 5],
+    [ 3, 5,  0, 5],
+    [ 0, 5,  0, 0],
+  ];
 
+  // Generates room model.
 
+  const room = RoomRenderer(rects);
 
+  // Creates VAO out of it.
 
+  const vao = create_vao(room);
 
+  // Modelview.
 
+  const modelview = mat4.translate([], mat4.create(), [0, 0, 0]);
 
-  // sheet
-  // Cторона куба, использующаяся для создания стены.
-  // Не изменяется сам, но изменяется в процессе создания стены.
+  // RENDER.
 
-  const VERTICES_LEN = 4;
-  const INDICES_LEN = 6;
+  g.render = function(timestamp, elapsed) {
 
-  const sheet = {
-    texuv: [],
-    coord: [],
-    normal: [],
-    tangent: [],
-    indices: [],
+    render_vao(vao, modelview, room.indices);
+
   };
 
-  // prepares sheet
-  {
-    for (let i = 0; i < VERTICES_LEN; ++i) {
-      sheet.texuv.push(WebGLu.CUBE.texcoords[i * 2 + 0],
-                       WebGLu.CUBE.texcoords[i * 2 + 1]);
-      sheet.coord.push(WebGLu.CUBE.coordinates[i * 3 + 0],
-                       WebGLu.CUBE.coordinates[i * 3 + 1],
-                       0);
-      sheet.normal.push(WebGLu.CUBE.normals[i * 3 + 0],
-                        WebGLu.CUBE.normals[i * 3 + 1],
-                        WebGLu.CUBE.normals[i * 3 + 2]);
-      sheet.tangent.push(WebGLu.CUBE.tangents[i * 3 + 0],
-                         WebGLu.CUBE.tangents[i * 3 + 1],
-                         WebGLu.CUBE.tangents[i * 3 + 2]);
-    }
 
-    for (let i = 0; i < INDICES_LEN; ++i) {
-      sheet.indices.push(WebGLu.CUBE.indices[i]);
-    }
-  }
+  ///////////////////////////////////////////////////////////////
 
-  // room
-  // Здесь заполняются данные для VAO.
-  // Данные копируются из sheet изменяясь с помощью transformMat4.
 
-  const texuv = [];
-  const coord = [];
-  const normal = [];
-  const tangent = [];
-  const indices = [];
-
-  let walls_pushed = 0;
-
-  function push_wall(mv, mr) {
-    mv = mv || mat4.create(); // modelview
-    mr = mr || mat4.create(); // rotation matrix
-
-    const rv = mat4.multiply([], mr, mv);
-
-    for (let vi = 0; vi < VERTICES_LEN; ++vi) {
-      texuv.push(...vec2.transformMat4([], arrpart(sheet.texuv, vi * 2, 2), mv));
-      coord.push(...vec3.transformMat4([], arrpart(sheet.coord, vi * 3, 3), rv));
-      normal.push(...arrpart(sheet.normal, vi * 3, 3));
-      tangent.push(...arrpart(sheet.tangent, vi * 3, 3));
-      // TODO: нормали и тангенты вычисляются неправильно
-    }
-
-    indices.push(...sheet.indices.map(e => e + walls_pushed * 4));
-
-    ++walls_pushed;
-  }
-
-  function create_vao() {
+  function create_vao(raw) {
     const vao = webgl.bind_vao();
-    webgl.bind_array_buffer(g.a_loc('a_texuv'), new Float32Array(texuv), 2, gl.FLOAT);
-    webgl.bind_array_buffer(g.a_loc('a_coord'), new Float32Array(coord), 3, gl.FLOAT);
-    webgl.bind_array_buffer(g.a_loc('a_normal'), new Float32Array(normal), 3, gl.FLOAT);
-    webgl.bind_array_buffer(g.a_loc('a_tangent'), new Float32Array(tangent), 3, gl.FLOAT);
-    webgl.bind_element_buffer(new Uint16Array(indices));
+    webgl.bind_array_buffer(g.a_loc('a_texuv'),   new Float32Array(raw.texcoords),   2, gl.FLOAT);
+    webgl.bind_array_buffer(g.a_loc('a_coord'),   new Float32Array(raw.coordinates), 3, gl.FLOAT);
+    webgl.bind_array_buffer(g.a_loc('a_normal'),  new Float32Array(raw.normals),     3, gl.FLOAT);
+    webgl.bind_array_buffer(g.a_loc('a_tangent'), new Float32Array(raw.tangents),    3, gl.FLOAT);
+    webgl.bind_element_buffer(new Uint16Array(raw.indices));
     return vao;
   }
 
-  function render_vao(vao, modelview) {
+
+  function render_vao(vao, modelview, indices) {
     gl.bindVertexArray(vao);
 
     gl.activeTexture(gl.TEXTURE0);
@@ -124,65 +80,4 @@ function Renderer(g) {
 
     gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
   }
-
-
-
-
-
-
-  const walls = [
-    [ 0, 0,  3, 0],
-    [ 3, 0,  3, 5],
-    [ 3, 5,  0, 5],
-    [ 0, 5,  0, 0],
-  ];
-
-
-
-  walls.forEach((w,i) => {
-    const dx = Math.abs(w[2] - w[0]);
-    const dy = Math.abs(w[3] - w[1]);
-    const scaler = dx || dy;
-    const rotate = Math.abs(dx) < Math.abs(dy);
-    let tx = Math.min(w[0], w[2]);
-    let ty = Math.min(w[1], w[3]);
-    if (rotate) [tx, ty] = [ty, -tx];
-
-    const mv = mat4.create();
-    mat4.translate(mv, mv, [tx, 0, ty]);
-    mat4.scale(mv, mv, [scaler, 1, 1]);
-
-    const mr = mat4.create();
-    if (rotate) mat4.rotateY(mr, mr, -Math.PI / 2 * Math.sign(dy));
-
-    push_wall(mv, mr);
-  });
-
-
-
-  const vao = create_vao();
-
-
-  const modelview = mat4.translate([], mat4.create(), [0, 0, 0]);
-
-  // render
-  g.render = function(timestamp, elapsed) {
-
-    // g.render_cube(modelview);
-
-    // WebGLu.draw_btn(
-    //   ctx, webgl.viewport, g.projection,
-    //   { coordinates: coord, normals: normal, tangents: tangent, bitangents: null, indices: indices },
-    // );
-
-    render_vao(vao, modelview);
-
-
-  };
-
-
-
-
-
-
 }
---
+++ js/rooms/js/roomrenderer.js
@@ -0,0 +1,61 @@
+//
+function RoomRenderer(rects) {
+  // VAO data.
+  const coordinates = [];
+  const texcoords   = [];
+  const tangents    = [];
+  const normals     = [];
+  const indices     = [];
+
+  let wi = 0; // see push_wall
+
+  // Pushes wall geometry into VAO data.
+  rects.forEach(e => push_wall(e));
+
+
+  /////////////////////////////////////////////////////
+
+
+  // Pushes wall geometry into VAO data.
+  function push_wall(wall) {
+    const dx = wall[2] - wall[0];
+    const dz = wall[3] - wall[1];
+    const is_along_x = !!dx;
+    const texscaler = Math.abs(dx||dz);
+    let dir; if (is_along_x) dir = dx > 0 ? 1 : 3; else dir = dz > 0 ? 0 : 2;
+    const x1 = wall[0];
+    const z1 = wall[1];
+    const x2 = x1 + dx;
+    const z2 = z1 + dz;
+
+    coordinates.push(x1, 0, z1, /**/ x2, 0, z2, /**/ x2, 1, z2, /**/ x1, 1, z1);
+    texcoords.push(0, 1, /**/ texscaler, 1, /**/ texscaler, 0, /**/ 0, 0);
+    indices.push(...[0, 1, 2, /**/ 2, 3, 0].map(e => e + (wi << 2)));
+
+    if (dir === 0) {
+      normals.push (-1,  0,  0, /**/ -1,  0,  0, /**/ -1,  0,  0, /**/ -1,  0,  0);
+      tangents.push( 0,  0, +1, /**/  0,  0, +1, /**/  0,  0, +1, /**/  0,  0, +1);
+    } else if (dir === 1) {
+      normals.push ( 0,  0, +1, /**/  0,  0, +1, /**/  0,  0, +1, /**/  0,  0, +1);
+      tangents.push(+1,  0,  0, /**/ +1,  0,  0, /**/ +1,  0,  0, /**/ +1,  0,  0);
+    } else if (dir === 2) {
+      normals.push (+1,  0,  0, /**/ +1,  0,  0, /**/ +1,  0,  0, /**/ +1,  0,  0);
+      tangents.push( 0,  0, -1, /**/  0,  0, -1, /**/  0,  0, -1, /**/  0,  0, -1);
+    } else if (dir === 3) {
+      normals.push ( 0,  0, -1, /**/  0,  0, -1, /**/  0,  0, -1, /**/  0,  0, -1);
+      tangents.push(-1,  0,  0, /**/ -1,  0,  0, /**/ -1,  0,  0, /**/ -1,  0,  0);
+    }
+
+    ++wi;
+  }
+
+
+  // Out.
+  return {
+    coordinates,
+    texcoords,
+    tangents,
+    normals,
+    indices,
+  }
+}
